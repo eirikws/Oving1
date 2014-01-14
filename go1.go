73b4cdccb3084a7b0c55eb94328e4f4a2ecf0726
eirikws <eirikws@stud.ntnu.no>
2014-01-14 23:40:55 +0100

    Update go1.go

--- go1.go
+++ go1.go
@@ -1,1 +1,29 @@
-aw
+package main
+
+import (
+    . "fmt"     // Using '.' to avoid prefixing functions with their package names
+    . "runtime" //   This is probably not a good idea for large projects...
+    . "time"
+)
+
+var i = 0
+
+func adder() {
+    for x := 0; x < 1000000; x++ {
+        i++
+    }
+}
+
+func decr() {
+    for x :=0 ; x<1000000; x++ {
+        i--
+    }
+}
+
+func main() {
+    GOMAXPROCS(NumCPU())
+    go adder()
+    go decr()
+    Sleep(100*Millisecond)
+    Println("Done:", i);
+}
